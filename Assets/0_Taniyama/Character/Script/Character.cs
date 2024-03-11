84e49a78c2cf0764567a91f74ed417c8c8456d48
enable01010 <132036160+enable01010@users.noreply.github.com>
2024-03-12 02:43:54 +0900

    プレイヤーのスクリプトをパーシャル化

---
+++ Assets/0_Taniyama/Character/Script/Character.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public partial class Character : Singleton<Character>
+{
+    Animator _animator;
+
+    //ステートに関係する処理
+    A_PlayerState state;
+    [SerializeField] IdleState idle;
+    [SerializeField] MoveState move;
+    [SerializeField] Fall fall;
+    [SerializeField] WallLandingMotion wallLanding;
+
+    //ステート間で保持したいデータ
+    [SerializeField] float downforce = 2.0f;
+    [SerializeField] float moveSpeed_Y = 20.0f;
+    [SerializeField] float moveSpeed_X = 16.0f;
+    [SerializeField] Transform _hoppingFrontPos;
+    [SerializeField,ReadOnly]Vector3 moveDir;
+    [SerializeField] GameObject _arrow;
+    [SerializeField] SpriteRenderer[] _visions;
+
+    //ステート関係無いデータ
+    [SerializeField,Range(0,1)] float freePartRait = 0.5f;
+    [SerializeField] float freePartRaitMoveSpeed = 0.01f;
+
+    public override void OnInitialize()
+    {
+        base.OnInitialize();
+
+        _animator = GetComponent<Animator>();
+
+        idle.OnInit(this);
+        move.OnInit(this);
+        fall.OnInit(this);
+        wallLanding.OnInit(this);
+
+        ChangeState(idle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        state.OnUpdate();
+    }
+
+    private void FixedUpdate()
+    {
+        state.OnFixedUpdate();
+
+        ControlFreePart();
+    }
+
+    private void ControlFreePart()
+    {
+        float bestRait = 0.5f;
+        bestRait = (moveDir.y > 0.01f) ? 1 : bestRait;
+        bestRait = (moveDir.y < -0.01f) ? 0 : bestRait;
+        
+        if(Mathf.Abs(bestRait - freePartRait) < freePartRaitMoveSpeed)
+        {
+            freePartRait = bestRait;
+        }
+        else if(bestRait > freePartRait)
+        {
+            freePartRait += freePartRaitMoveSpeed;
+        }
+        else if(bestRait < freePartRait)
+        {
+            freePartRait -= freePartRaitMoveSpeed;
+        }
+        _animator.SetFloat("fall", freePartRait);
+    }
+
+    /// <summary>
+    /// ステートを変更する処理
+    /// </summary>
+    /// <param name="next"></param>
+    private void ChangeState(A_PlayerState next)
+    {
+        state?.OnExit();
+        state = next;
+        state.OnEnter();
+    }
+
+    [System.Serializable]
+    public class A_PlayerState : I_State
+    {
+        protected Character character;
+        public void OnInit(Character character)
+        {
+            this.character = character;
+        }
+
+        public virtual void OnEnter() { }
+        public virtual void OnUpdate() { }
+        public virtual void OnFixedUpdate() { }
+        public virtual void OnExit() { }
+
+    }
+
+    
+    private LinecastVec CheckLinecastVec(Vector3 point)
+    {
+        LinecastVec answer = LinecastVec.other;
+
+        //変数の定義
+        const float range = 0.2f;
+        bool isLeftVertical = false;
+        bool isRightVertical = false;
+        bool isUpHolizontal = false;
+        bool isDownHolizontal = false;
+
+        //頂点の計算
+        Vector2 pos_LeftUp = point;
+        pos_LeftUp.x -= range;
+        pos_LeftUp.y += range;
+        Vector2 pos_RightUp = point;
+        pos_RightUp.x += range;
+        pos_RightUp.y += range;
+        Vector2 pos_LeftDown = point;
+        pos_LeftDown.x -= range;
+        pos_LeftDown.y -= range;
+        Vector2 pos_RightDown = point;
+        pos_RightDown.x += range;
+        pos_RightDown.y -= range;
+
+        //判定のチェック
+        if (Physics2D.Linecast(pos_LeftUp, pos_LeftDown))
+        {
+            isLeftVertical = true;
+        }
+        if (Physics2D.Linecast(pos_RightUp, pos_RightDown))
+        {
+            isRightVertical = true;
+        }
+        if (Physics2D.Linecast(pos_LeftUp, pos_RightUp))
+        {
+            isUpHolizontal = true;
+        }
+        if (Physics2D.Linecast(pos_LeftDown, pos_RightDown))
+        {
+            isDownHolizontal = true;
+        }
+
+        //横の場合
+        if ((isLeftVertical == true || isRightVertical == true) && 
+            (isUpHolizontal == false || isDownHolizontal && false))
+        {
+            answer = LinecastVec.horizontal;
+        }
+
+        //縦の場合
+        if ((isUpHolizontal == true || isDownHolizontal == true) &&
+            (isLeftVertical == false || isRightVertical == false))
+        {
+            answer = LinecastVec.vertical;
+        }
+
+        return answer;
+    }
+
+    public enum LinecastVec
+    {
+        vertical,
+        horizontal,
+        other,
+    }
+}
---
+++ Assets/0_Taniyama/Character/Script/Fall.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Character : Singleton<Character>
+{
+    [System.Serializable]
+    public class Fall : A_PlayerState
+    {
+        [SerializeField, Range(0, 1)] float slow = 0.5f;
+        float downforce;
+        float moveSpeed_Y;
+        float moveSpeed_X;
+        Transform _hoppingFrontPos;
+
+        public override void OnEnter()
+        {
+            this.downforce = character.downforce;
+            this.moveSpeed_Y = character.moveSpeed_Y;
+            this.moveSpeed_X = character.moveSpeed_X;
+            this._hoppingFrontPos = character._hoppingFrontPos;
+        }
+
+        public override void OnFixedUpdate()
+        {
+            //ë¨ìxí≤êÆèàóù
+            Vector3 moveDir = character.moveDir;
+            moveDir.x *= moveSpeed_X;
+            moveDir.y *= moveSpeed_Y;
+            moveDir *= Time.fixedDeltaTime;
+
+            //Ç†ÇΩÇËîªíËèàóù
+            RaycastHit2D hit = Physics2D.Linecast(_hoppingFrontPos.position, _hoppingFrontPos.position + moveDir);
+            if (hit)
+            {
+                Vector3 hitPos = hit.point;
+                Vector3 dir = hitPos - _hoppingFrontPos.position;
+                Vector3 goalPos = character.transform.position + dir;
+                goalPos.y = hit.point.y + 0.2f;
+                character.transform.position = goalPos;
+
+                LinecastVec vec = character.CheckLinecastVec(hit.point);
+                switch (vec)
+                {
+                    case LinecastVec.horizontal:
+                        character.ChangeState(character.idle);
+                        return;
+                    case LinecastVec.other:
+                        character.ChangeState(character.idle);
+                        return;
+                }
+            }
+
+            Move(moveDir);
+        }
+
+        /// <summary>
+        /// à⁄ìÆèàóù
+        /// </summary>
+        private void Move(Vector3 moveDir)
+        {
+            character.transform.position += moveDir;
+            character.moveDir.y -= downforce * Time.fixedDeltaTime * slow;
+        }
+    }
+
+}
