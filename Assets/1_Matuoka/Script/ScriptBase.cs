626ffe983bf0882722f385e4074d774ca883e56e
matsuokasvga22 <145432161+matsuokasvga22@users.noreply.github.com>
2024-03-12 02:44:45 +0900

    初期設定中だよ！！

---
+++ Assets/1_Matuoka/Script/ScriptBase.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptBase : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private GameObject playerObject;
+
+    #endregion
+
+
+    #region MonoBehaviourMethod
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    #endregion
+
+
+    #region CustomMethod
+
+    private void CustomMethod()
+    {
+        
+    }
+
+    #endregion
+}
